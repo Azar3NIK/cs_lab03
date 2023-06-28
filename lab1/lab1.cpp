2debc2f6f4090c4333d86fdaa05c9555259f3a2f
Nikita (AzarenkovNS) <azarenkov.n@gmail.com>
2023-06-28 19:13:06 +0300

    code: адаптирован вывод гистограммы

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -85,23 +85,34 @@ void show_histogram_text(const vector<size_t>& bins, const vector<double>& numbe
     }
 };
 
-void show_histogram_svg(const vector<size_t>& bins) {
+void show_histogram_svg(const vector<size_t>& bins, const vector<double>& numbers) {
 
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
 
-    size_t max_count = bins[0];
+    double min, max;
+    //Поиск минимального и максимального
+    find_minmax(numbers, min, max);
+
     size_t bin_count = bins.size();
+
+    double bin_size = (max - min) / bin_count;
+
+    size_t max_count = bins[0];
     for (size_t i = 1; i < bin_count; i++) {
         if (max_count < bins[i])
             max_count = bins[i];
     }
 
     double top = 0;
+    size_t counter = 0;
     for (size_t bin : bins) {
         const double bin_width = static_cast<size_t>(IMAGE_WIDTH * 0.5 * (static_cast<double>(bin) / max_count));
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "#6699FF", get_random_color());
-        top += BIN_HEIGHT;
+        svg_text(TEXT_LEFT + EDGE_WIDTH, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH + EDGE_WIDTH, top, bin_width, BIN_HEIGHT, "#6699FF", get_random_color());
+        auto hi = min + (static_cast<double>(counter) + 1) * bin_size;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE + EDGE_HEIGHT, svg_edges(hi));
+        top += (BIN_HEIGHT + EDGE_HEIGHT);
+        counter++;
     }
 
     svg_end();
@@ -137,7 +148,7 @@ int main()
     const auto bins = make_histogram(numbers,bin_count);
 
    // show_histogram_text(bins, numbers);
-    show_histogram_svg(bins);
+    show_histogram_svg(bins, numbers);
 
     return 0;
 }
