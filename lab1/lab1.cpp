e7640ce0eb6416f52e13a6e81f04f9ab61b08cb6
Nikita (AzarenkovNS) <azarenkov.n@gmail.com>
2023-06-29 00:10:40 +0300

    code: организована упорядоченная передача данных

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -10,12 +10,35 @@ using namespace std;
 const size_t SCREEN_WIDTH = 80;
 const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 4;
 
+struct Input {
+    vector<double> numbers;
+    size_t bin_count;
+};
+
 vector<double> input_numbers(istream& in, size_t count) {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++) {
         in >> result[i];
     }
     return result;
+}
+
+Input read_input(istream& in) {
+    Input data;
+
+    cerr << "Enter number count: ";
+    size_t number_count;
+    in >> number_count;
+
+    cerr << "Enter numbers: ";
+    data.numbers = input_numbers(in, number_count);
+
+    size_t bin_count;
+    cerr << "Enter bin count: ";
+    in >> bin_count;
+    data.bin_count = bin_count;
+
+    return data;
 }
 
 vector<size_t> make_histogram(const vector<double>& numbers, size_t bin_count) {
@@ -121,34 +144,13 @@ void show_histogram_svg(const vector<size_t>& bins, const vector<double>& number
 
 int main()
 {
-    size_t number_count;
-    cerr << "Enter number count: ";
-    cin >> number_count;
 
-    if (number_count <= 0) {
-        cerr << "number count cannot be equal or below zero";
-        return 0;
-    }
+    Input data = read_input(cin);
 
-    const auto numbers = input_numbers(cin, number_count);
-
-    if (numbers.size() == 0) {
-        cerr << "numbers cannot be empty";
-    }
-
-    size_t bin_count;
-    cerr << "Enter bin count: ";
-    cin >> bin_count;
-
-    if (bin_count <= 0) {
-        cerr << "bin count cannot be equal or below zero";
-        return 0;
-    }
-
-    const auto bins = make_histogram(numbers,bin_count);
+    const auto bins = make_histogram(data.numbers, data.bin_count);
 
    // show_histogram_text(bins, numbers);
-    show_histogram_svg(bins, numbers);
+    show_histogram_svg(bins, data.numbers);
 
     return 0;
 }
